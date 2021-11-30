2826e60a0db4b99b530d22b7419e1a37c4db379b
aniket <aniketavinashmore343@gmail.com>
2021-11-30 20:40:56 +0530

    remove husky

--- src/app.ts
+++ src/app.ts
@@ -2,30 +2,35 @@ import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 import morgan from 'morgan'
-import fs from 'fs';
-import path from 'path';
+import fs from 'fs'
+import path from 'path'
 
 import { userRouter } from './routes/users'
 import { errorHandler } from './lib/middleware/errorHandler'
 import { notFoundHandler } from './lib/middleware/notFoundHandler'
 import { getEnv } from './lib/utils'
-import { appConfig } from './config/appConfig';
+import { appConfig } from './config/appConfig'
 
 export const app = express()
 
 const NODE_ENV = getEnv('NODE_ENV', 'development')
-const logFileStream = fs.createWriteStream(path.join(appConfig.properties.ROOT_DIR, "logs/access.log"), {
-  flags: "a"
-})
+const logFileStream = fs.createWriteStream(
+  path.join(appConfig.properties.ROOT_DIR, 'logs/access.log'),
+  {
+    flags: 'a',
+  }
+)
 
 app.use(cors())
 app.use(helmet())
-if (NODE_ENV == "development") {
+if (NODE_ENV == 'development') {
   app.use(morgan('dev'))
 }
-app.use(morgan("combined", {
-  stream: logFileStream
-}))
+app.use(
+  morgan('combined', {
+    stream: logFileStream,
+  })
+)
 app.use(express.json())
 
 app.get('/health', (req, res) => {
--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,10 +1,10 @@
-import path from 'path';
+import path from 'path'
 
-const ROOT_DIR = path.join(__dirname, "..", "..");
-console.log(ROOT_DIR);
+const ROOT_DIR = path.join(__dirname, '..', '..')
+console.log(ROOT_DIR)
 
 export const appConfig = {
   properties: {
-    ROOT_DIR: ROOT_DIR
-  }
+    ROOT_DIR: ROOT_DIR,
+  },
 }
