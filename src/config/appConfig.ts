b484d6d324384f44e405d7a02c8ec277dca998ef
aniket <aniketavinashmore343@gmail.com>
2021-12-02 19:16:17 +0530

    setup app and env config

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,13 +1,36 @@
-import path from 'path'
-import { getEnv } from '../lib/utils'
+import path from 'path';
+import configFile from './config.json';
 
+
+const properties:{[key:string]:string} = {
+  ...configFile
+}
+
+/**
+ * @description returns config variables, if not found throws error
+ */
+function get(key:string):string {
+  const val = properties[key];
+  if (val !== undefined) {
+    return properties[key];
+  } else {
+    throw Error(`config variable ${key} not found`);
+  }
+}
+
+/**
+ * @description sets config variable
+ */
+function set(key:string, val:string) {
+  properties[key] = val;
+}
+
+// setting values
 const ROOT_DIR = path.join(__dirname, '..', '..')
+set('ROOT_DIR', ROOT_DIR);
+set('LOG_DIR', path.join(ROOT_DIR, 'logs'));
 
-export const appConfig = {
-  ROOT_DIR: ROOT_DIR,
-  LOG_DIR: path.join(ROOT_DIR, "logs"),
-  env: {
-    NODE_ENV: getEnv('NODE_ENV', 'development'),
-    PORT: getEnv('PORT', '8080'),
-  },
+export default {
+  get,
+  set
 }
--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import { config } from 'dotenv'
 config()
 
 import { app } from './app'
-import { appConfig } from './config/appConfig'
+import envConfig from './config/envConfig';
+import appConfig from './config/appConfig';
 
-const PORT = appConfig.env.PORT
-const NODE_ENV = appConfig.env.NODE_ENV
+const PORT = envConfig.get('PORT');
+const NODE_ENV = envConfig.get("NODE_ENV");
+const name = appConfig.get('APP_NAME');
 
 app.listen(PORT, () => {
-  console.log(`server started - port: ${PORT}, mode: ${NODE_ENV}`)
+  console.log(`${name}\nserver started - port: ${PORT}, mode: ${NODE_ENV}`)
 })
