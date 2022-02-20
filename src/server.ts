f9eb37b65f2663f58563484c96269e6532397115
aniket <aniketavinashmore343@gmail.com>
2022-02-20 16:22:59 +0530

    fix bug with port

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ config()
 import { app } from './app'
 import appConfig from './config/appConfig'
 
-const PORT = appConfig.env.NODE_ENV;
+const PORT = appConfig.env.PORT;
 const NODE_ENV = appConfig.env.NODE_ENV;
 const name = appConfig.properties.APP_NAME;
 
