277232778cd554c59745a5e0f7d82dddc01848f6
aniket <aniketavinashmore343@gmail.com>
2022-04-17 19:37:27 +0530

    fix: refactor

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,21 @@ config()
 import { app } from './app'
 import { configService } from './config'
 
-const PORT = configService.getConfig('PORT')
-const NODE_ENV = configService.getConfig('NODE_ENV')
-const name = configService.getConfig('APP_NAME')
+async function bootstrap() {
+  const PORT = configService.getConfig('PORT')
+  const NODE_ENV = configService.getConfig('NODE_ENV')
+  const name = configService.getConfig('APP_NAME')
 
-app.listen(PORT, () => {
-  console.log(
-    `${name}\nserver started on http://localhost:${PORT} \nport: ${PORT}, mode: ${NODE_ENV}`
-  )
+  app.listen(PORT, () => {
+    console.log(
+      `${name}\nserver started on http://localhost:${PORT} \nport: ${PORT}, mode: ${NODE_ENV}`
+    )
+  })
+}
+
+bootstrap().catch((err) => {
+  if (err) {
+    console.log(err)
+    process.exit(1)
+  }
 })
